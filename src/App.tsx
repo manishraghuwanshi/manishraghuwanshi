b7743d9d9609a9c6f8e4caad5491632f24a139a8
manishraghuwanshi <manishraghuwanshiofficial@gmail.com>
2026-09-16 06:30:52 +0530

    updated App and main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import { PageTransition } from "./components/effects";
+import Breadcrumbs from "./components/breadcrumbs/BreakCrumbs";
+import ErrorBoundary from "./components/ui/error/ErrorBoundary";
+import HeaderErrorBoundary from "./components/ui/error/HeaderErrorBoundary";
+import FooterErrorBoundary from "./components/ui/error/FooterErrorBoundary";
 
-const App = () => {
+export default function App() {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === "/";
+
   return (
-    <div>App</div>
-  )
-}
+    <div className="min-h-screen bg-[#000000] text-white">
+      <HeaderErrorBoundary>
+        <Header />
+      </HeaderErrorBoundary>
 
-export default App
+      <main className={isHomePage ? "" : "pt-20"}>
+        {!isHomePage && <Breadcrumbs />}
+
+        <ErrorBoundary>
+          <PageTransition>
+            <Outlet />
+          </PageTransition>
+        </ErrorBoundary>
+      </main>
+      <FooterErrorBoundary>
+        <Footer />
+      </FooterErrorBoundary>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { RouterProvider } from "react-router-dom";
+
+import { AppRouter } from "./router/AppRouter";
+import ErrorBoundary from "./components/ui/error/ErrorBoundary";
+
+import "./styles/globals.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!,
+).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <RouterProvider router={AppRouter} />
+    </ErrorBoundary>
+  </React.StrictMode>,
+);
